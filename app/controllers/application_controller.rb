30d3153f0a84ad974c383f8d9508d88cd0cde028
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-09 22:57:46 -0500

    Add redirection when the user sign in or sign out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
+
+  def after_sign_in_path_for(user)
+    return user_path(user)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    return root_path
+  end
 end
