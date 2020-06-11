3b8746774c6811df406289c3dbeddef50f0d361f
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-11 10:14:44 -0500

    Add associations between Groups and Users.

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,9 @@
 
 class Group < ApplicationRecord
   has_many :drinks
+  belongs_to :user
 
   scope :by_user, lambda { |user_id|
-    where(user_id: user_id).order(name: :asc)
+    Group.where(user_id: user_id).order(name: :asc).includes(:drinks)
   }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :drinks, dependent: :destroy, foreign_key: :author_id
+  has_many :groups
 
   def sum_drinks(grouped = true)
     if grouped
