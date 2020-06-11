2782281a03874c4b9f3682af5685106fed28dcef
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-10 23:21:36 -0500

    Change seed and drink helper to start using groups.

--- app/helpers/drinks_helper.rb
+++ app/helpers/drinks_helper.rb
@@ -4,9 +4,9 @@ module DrinksHelper
   def drinks_total
     total = 'You have to drink'
     if @total_drinks > 1000
-      total.concat(" #{@total_drinks / 1000}L")
+      "#{total} #{@total_drinks / 1000}L"
     else
-      total.concat(" #{@total_drinks}ml")
+      "#{total} #{@total_drinks}ml"
     end
   end
 
