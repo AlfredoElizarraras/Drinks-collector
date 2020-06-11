77284f1f1ff4dca52591caaf16c478b75b654480
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-11 06:48:06 -0500

    Add User reference to Group table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-g = Group.create(name: "Group 1", icon: 'https://img.icons8.com/color/48/000000/wine-bottle.png')
-10.times do |index|
-  u = User.create(name: "User #{index + 1}", email: "user_#{index + 1}@mail.com", password: '123456')
-  Drink.create(name: "Drink #{index + 1}", amount: 760, author_id: u.id, group_id: g.id)
-  Drink.create(name: "Drink 2.#{index + 1}", amount: 760, author_id: u.id, group_id: g.id)
-  if index.odd?
-    Drink.create(name: "Drink #{index + 1}.1", amount: 760, author_id: u.id)
-  else
-    Drink.create(name: "Drink #{index + 1}.2", amount: 760, author_id: u.id, group_id: g.id)
-  end
-end
+# g = Group.create(name: "Group 1", icon: 'https://img.icons8.com/color/48/000000/wine-bottle.png')
+# 10.times do |index|
+#   u = User.create(name: "User #{index + 1}", email: "user_#{index + 1}@mail.com", password: '123456')
+#   Drink.create(name: "Drink #{index + 1}", amount: 760, author_id: u.id, group_id: g.id)
+#   Drink.create(name: "Drink 2.#{index + 1}", amount: 760, author_id: u.id, group_id: g.id)
+#   if index.odd?
+#     Drink.create(name: "Drink #{index + 1}.1", amount: 760, author_id: u.id)
+#   else
+#     Drink.create(name: "Drink #{index + 1}.2", amount: 760, author_id: u.id, group_id: g.id)
+#   end
+# end
