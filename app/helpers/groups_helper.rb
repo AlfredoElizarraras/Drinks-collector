b2c91befbd6feea3e50b82c2d6cdbfa78c4c4618
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-11 22:30:39 -0500

    Fix show of non group image when the user do not provide any icon. Add integration test for user login and creation of drinks and groups.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,8 +2,8 @@
 
 module GroupsHelper
   def show_group_icon(group)
-    if !group.nil?
-      render html: image_tag(group.icon) unless group.icon.nil?
+    unless group.icon == ''
+      render html: image_tag(group.icon)
     else
       render html: image_tag('no_group.png')
     end
