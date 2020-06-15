22dae94249b4a4e1936f95c2d9935f22d7a4716f
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-15 16:33:40 -0500

    Change scope for github initializer.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,33 +1,32 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def github
-      omniauth_redirection('github')
-    end
-  
-    def google_oauth2
-      omniauth_redirection('google')
-    end
-  
-    def facebook
-      omniauth_redirection('facebook')
-    end
-  
-    def failure
-      redirect_to root_path
-    end
-  
-    private
-  
-    def omniauth_redirection(provider)
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
-      else
-        session["devise.#{provider}_data"] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
-      end
+  def github
+    omniauth_redirection('github')
+  end
+
+  def google_oauth2
+    omniauth_redirection('google')
+  end
+
+  def facebook
+    omniauth_redirection('facebook')
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def omniauth_redirection(provider)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
     end
   end
-  
+end
