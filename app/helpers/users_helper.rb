bf91e3b9e133c5a55d739229c6173219f7fefb39
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-15 19:02:15 -0500

    Fix rubocop offenses.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,8 @@
 module UsersHelper
   def profile_image(user)
     if user.gravatar_url.nil?
-      render html: image_tag('https://image.flaticon.com/icons/svg/2919/2919573.svg', alt: 'profile image', class: 'rounded-circle')
+      render html: image_tag('https://image.flaticon.com/icons/svg/2919/2919573.svg',
+                             alt: 'profile image', class: 'rounded-circle')
     else
       render html: image_tag(user.gravatar_url, alt: "#{user.name} profile image", class: 'rounded-circle')
     end
